3d05a5c5a5ee207314b172e22002b3532e9acb7a
Ревякин Андрей Владимирович <andrevyakin@gmail.com>
2021-04-04 00:08:01 +0300

    Структура БД

---
+++ Bookinist.DAL/Entityes/Deal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using Bookinist.DAL.Entityes.Base;
+
+namespace Bookinist.DAL.Entityes
+{
+    public class Deal : Entity
+    {
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+        
+        public virtual ICollection<Book> Books { get; set; }
+        public virtual Seller Seller { get; set; }
+        public virtual Buyer Buyer { get; set; }
+
+    }
+}
