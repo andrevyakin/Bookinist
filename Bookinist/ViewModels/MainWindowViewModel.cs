f1cb103cd245964d83a1953db0365b7aeb4730fe
Ревякин Андрей Владимирович <andrevyakin@gmail.com>
2021-04-03 19:50:36 +0300

    Модель главного окна и локатор моделей представления

---
+++ Bookinist/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathCore.WPF.ViewModels;
+
+namespace Bookinist.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        #region Title - string Заголовок
+
+        /// <summary>Заголовок</summary>
+        private string _Title = "Главное окно программы";
+
+        /// <summary>Заголовок</summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
+    }
+}
