73aa838f3ddc70241217df7044883398848a171f
Ревякин Андрей Владимирович <andrevyakin@gmail.com>
2021-04-04 14:35:31 +0300

    Реализация специализированных репозиториев

--- Bookinist.DAL/Entityes/Book.cs
+++ Bookinist.DAL/Entityes/Book.cs
@@ -6,5 +6,7 @@ namespace Bookinist.DAL.Entityes
     public class Book : NamedEntity
     {
         public virtual Category Category { get; set; }
+
+        public override string ToString() => $"Книга {Name}";
     }
 }
